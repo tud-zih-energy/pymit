b9e722bdd67a7d372112aa3b3a7f6258ab5dcd03
Fabian Koller <fabian.koller@mailbox.tu-dresden.de>
2020-05-23 17:53:42 +0200

    Introduce replacement for numpy histogramdd calculation

--- mephisto/mephisto.cpp
+++ mephisto/mephisto.cpp
@@ -1,9 +1,13 @@
 #include <algorithm>
 #include <cmath>
 #include <limits>
+#include <memory>
+
+#include <iostream>
 
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>
 
 /**
@@ -33,7 +37,6 @@ histogram(PyObject *self, PyObject *args, PyObject *kwds) {
     PyArrayObject *bin_edges = nullptr;
 
     /* helpers */
-    bool a_ref = false;
     PyArrayObject *a_np = nullptr;
     PyArrayObject *bins_np = nullptr;
     bool bin_edges_arg = false;
@@ -52,24 +55,16 @@ histogram(PyObject *self, PyObject *args, PyObject *kwds) {
     if (!PyArg_ParseTupleAndKeywords(args, kwds, "O|O(ff)pOp", kwlist, &a, &bins, &range_lower, &range_upper, &normed, &weights, &density))
         goto fail;
 
-    if (normed || weights || density)
+    if (normed || range_lower != -std::numeric_limits< double >::infinity() || range_upper != std::numeric_limits< double >::infinity() || weights || density)
     {
-        PyErr_SetString(PyExc_NotImplementedError, "Supported arguments: (a)");
+        PyErr_SetString(PyExc_NotImplementedError, "Supported arguments: (a, bins)");
         goto fail;
     }
 
     /* obtain ndarray behind `a` */
-    if (PyArray_CHKFLAGS(reinterpret_cast< PyArrayObject* >(a), NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED))
-    {
-        a_ref = true;
-        Py_INCREF(a);
-        a_np = reinterpret_cast< PyArrayObject* >(a);
-    } else
-    {
-        a_np = reinterpret_cast< PyArrayObject* >(PyArray_FROM_OTF(a, NPY_DOUBLE, NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED));
-        if (!a_np)
-            goto fail;
-    }
+    a_np = reinterpret_cast< PyArrayObject* >(PyArray_FROM_OTF(a, NPY_DOUBLE, NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED));
+    if (!a_np)
+        goto fail;
 
     /* process `bins` */
     if (bins)
@@ -98,10 +93,9 @@ histogram(PyObject *self, PyObject *args, PyObject *kwds) {
     bin_edges_dims = hist_dims + 1;
 
     /* allocate output arrays */
-    hist = reinterpret_cast< PyArrayObject* >(PyArray_SimpleNew(1, &hist_dims, weights ? PyArray_TYPE(weights_np) : NPY_INT64));
+    hist = reinterpret_cast< PyArrayObject* >(PyArray_ZEROS(1, &hist_dims, weights ? PyArray_TYPE(weights_np) : NPY_INT64, 0));
     if (!hist)
         goto fail;
-    PyArray_FILLWBYTE(hist, 0);
     bin_edges = reinterpret_cast< PyArrayObject* >(PyArray_SimpleNew(1, &bin_edges_dims, NPY_DOUBLE));
     if (!bin_edges)
         goto fail;
@@ -110,10 +104,7 @@ histogram(PyObject *self, PyObject *args, PyObject *kwds) {
     a_data = (double *)PyArray_DATA(a_np);
     if (!a_data)
         goto fail;
-
-    a_length = 1;
-    for (int i = 0; i < PyArray_NDIM(a_np); ++i)
-        a_length *= PyArray_DIM(a_np, i);
+    a_length = PyArray_SIZE(a_np);
 
     bin_edges_data = (double *)PyArray_DATA(bin_edges);
     if (!bin_edges_data)
@@ -130,8 +121,10 @@ histogram(PyObject *self, PyObject *args, PyObject *kwds) {
     {
         for (npy_intp i = 0; i < a_length; ++i)
         {
-            a_min = std::fmin(a_min, a_data[i]);
-            a_max = std::fmax(a_max, a_data[i]);
+            if (a_data[i] < a_min)
+                a_min = a_data[i];
+            if (a_data[i] > a_max)
+                a_max = a_data[i];
         }
         hist_bin_width = (a_max-a_min)/hist_dims;
 
@@ -159,23 +152,247 @@ histogram(PyObject *self, PyObject *args, PyObject *kwds) {
     goto success;
 
 success:
-    if (a_ref)
-        Py_DECREF(a);
-    else
-        Py_DECREF(a_np);
-     Py_XDECREF(bins_np);
+    Py_XDECREF(bins_np);
+    Py_DECREF(a_np);
     return Py_BuildValue("NN", hist, bin_edges);
 
 fail:
-    Py_XDECREF(hist);
-    Py_XDECREF(bin_edges);
-    Py_XDECREF(a_np);
     Py_XDECREF(bins_np);
+    Py_XDECREF(a_np);
+    Py_XDECREF(bin_edges);
+    Py_XDECREF(hist);
+    return NULL;
+}
+
+/**
+ * @brief Compute the multidimensional histogram of some data.
+ *
+ * @see https://numpy.org/doc/stable/reference/generated/numpy.histogramdd.html
+ * @see https://github.com/numpy/numpy/blob/v1.18.1/numpy/lib/histograms.py#L945-L1123
+ *
+ * @param self
+ * @param args
+ * @return PyObject*
+ */
+static PyObject*
+histogramdd(PyObject *self, PyObject *args, PyObject *kwds) {
+    /* input */
+    PyObject *sample = nullptr;
+    PyObject *bins = nullptr;
+    PyObject *range = nullptr;
+    bool normed;
+    PyObject *weights = nullptr;
+    bool density;
+    static char *kwlist[] = {"sample", "bins", "range", "normed", "weights", "density", NULL};
+
+    /* output */
+    PyArrayObject *H = nullptr;
+    PyObject *edges = nullptr;
+
+    /* helpers */
+    PyArrayObject *sample_np = nullptr;
+    npy_intp sample_N;
+    npy_intp sample_D;
+    npy_intp sample_length;
+    std::unique_ptr< npy_intp[] > H_dims;
+    npy_intp bins_;
+    PyArrayObject *bins_np = nullptr;
+    double *bins_data = nullptr;
+    bool bin_edges_arg = false;
+    std::unique_ptr< double[] > sample_min = nullptr;
+    std::unique_ptr< double[] > sample_max = nullptr;
+    std::unique_ptr< PyObject*[] > bin_edges_np = nullptr;
+    double *sample_data = nullptr;
+    std::unique_ptr< double[] > hist_bin_width = nullptr;
+    double *bin_edges_data = nullptr;
+    npy_intp bin_edges_dims;
+    double *H_data;
+
+    /* argument parsing */
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "O|OOpOp", kwlist, &sample, &bins, &range, &normed, &weights, &density))
+        goto fail;
+
+    if (range || normed || weights || density)
+    {
+        PyErr_SetString(PyExc_NotImplementedError, "Supported arguments: (sample)");
+        goto fail;
+    }
+
+    /* obtain ndarray behind `sample` */
+    if (!PyArray_CheckExact(sample))
+    {
+        PyErr_SetString(PyExc_TypeError, "Only array-form of `sample` is allowed");
+        goto fail;
+    }
+    sample_np = reinterpret_cast< PyArrayObject* >(PyArray_FROM_OTF(sample, NPY_DOUBLE, NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED));
+    if (!sample_np)
+        goto fail;
+    if (PyArray_NDIM(sample_np) != 2)
+    {
+        PyErr_SetString(PyExc_ValueError, "`sample` array must be 2D");
+        goto fail;
+    }
+    sample_N = PyArray_DIM(sample_np, 0);
+    sample_D = PyArray_DIM(sample_np, 1);
+    H_dims = std::make_unique< npy_intp[] >(sample_D);
+
+    /* process `bins` */
+    if (bins)
+    {
+        if (PyArray_IsIntegerScalar(bins)) // The number of bins for all dimensions (nx=ny=…=bins).
+        {
+            bins_ = PyLong_AsLong(bins);
+            for (npy_intp i = 0; i < sample_D; ++i)
+                H_dims[i] = bins_;
+        }
+        else
+        {
+            bins_np = reinterpret_cast< PyArrayObject* >(PyArray_FROM_OT(bins, NPY_DOUBLE));
+            if (!bins_np)
+                goto fail;
+
+            if (PyArray_NDIM(bins_np) == 1) // The number of bins for each dimension (nx, ny, … =bins)
+            {
+                if (PyArray_DIM(bins_np, 0) != sample_D)
+                {
+                    PyErr_SetString(PyExc_ValueError, "The dimension of `bins` must be equal to the dimension of `sample`");
+                    goto fail;
+                }
+                bins_data = (double *)PyArray_DATA(bins_np);
+                for (npy_intp i = 0; i < sample_D; ++i)
+                    H_dims[i] = bins_data[i];
+            }
+            else // A sequence of arrays describing the monotonically increasing bin edges along each dimension.
+            {
+                bin_edges_arg = true;
+                /*
+                if (PyArray_NDIM(bins_np) != 2)
+                {
+                    PyErr_SetString(PyExc_ValueError, "`bins` array must be 2D");
+                    goto fail;
+                }
+                if (PyArray_DIM(bins_np, 0) != sample_D)
+                {
+                    PyErr_SetString(PyExc_ValueError, "The dimension of `bins` must be equal to the dimension of `sample`");
+                    goto fail;
+                }
+
+                for (npy_intp i = 0; i < sample_D; ++i)
+                    H_dims = ...;
+                */
+            }
+        }
+    } else
+    {
+        for (npy_intp i = 0; i < sample_D; ++i)
+            H_dims[i] = 10;
+    }
+
+    /* allocate output arrays */
+    H = reinterpret_cast< PyArrayObject* >(PyArray_ZEROS(sample_D, H_dims.get(), NPY_DOUBLE, 0));
+    if (!H)
+        goto fail;
+    edges = PyList_New(sample_D);
+    if (!edges)
+        goto fail;
+    bin_edges_np = std::make_unique< PyObject*[] >(sample_D);
+    for (npy_intp i = 0; i < sample_D; ++i)
+    {
+        H_dims[i] += 1;
+        bin_edges_np[i] = PyArray_SimpleNew(1, &H_dims[i], NPY_DOUBLE);
+        if (!bin_edges_np[i])
+            goto fail;
+        PyList_SetItem(edges, i, bin_edges_np[i]);
+        H_dims[i] -= 1;
+    }
+
+    /* workwörk */
+    sample_data = (double *)PyArray_DATA(sample_np);
+    if (!sample_data)
+        goto fail;
+    sample_length = PyArray_SIZE(sample_np);
+    if (bin_edges_arg)
+    {
+        PyErr_SetString(PyExc_NotImplementedError, "Arbitrarily spaced bins not supported yet");
+        goto fail;
+    } else
+    {
+        sample_min = std::make_unique< double[] >(sample_D);
+        sample_max = std::make_unique< double[] >(sample_D);
+        for (npy_intp i = 0; i < sample_length; ++i)
+        {
+            auto dim = i % sample_D;
+            if (sample_data[i] < sample_min[dim])
+                sample_min[dim] = sample_data[i];
+            if (sample_data[i] > sample_max[dim])
+                sample_max[dim] = sample_data[i];
+        }
+        hist_bin_width = std::make_unique< double[] >(sample_D);
+        for (npy_intp i = 0; i < sample_D; ++i)
+        {
+            hist_bin_width[i] = (sample_max[i]-sample_min[i])/H_dims[i];
+
+            bin_edges_data = (double *)PyArray_DATA(reinterpret_cast< PyArrayObject* >(bin_edges_np[i]));
+            bin_edges_dims = H_dims[i] + 1;
+            if (!bin_edges_data)
+                goto fail;
+            bin_edges_data[0] = sample_min[i];
+            for (npy_intp j = 1; j < bin_edges_dims-1; ++j)
+                bin_edges_data[j] = sample_min[i] + hist_bin_width[i] * j;
+            bin_edges_data[bin_edges_dims-1] = sample_max[i];
+        }
+    }
+
+    H_data = (double *)PyArray_DATA(H);
+    if (bin_edges_arg)
+    {
+        PyErr_SetString(PyExc_NotImplementedError, "Arbitrarily spaced bins not supported yet");
+        goto fail;
+    } else
+    {
+        for (npy_intp i = 0; i < sample_length; i += sample_D)
+        {
+            npy_intp bin = 0;
+            for (npy_int j = 0; j < sample_D; ++j)
+            {
+                auto bin_ = static_cast< npy_intp >((sample_data[i+j] - sample_min[j]) / hist_bin_width[j]);
+                bin_ = std::min(bin_, H_dims[j]-1);
+                bin += bin_;
+                if (j != sample_D - 1)
+                    bin *= H_dims[j+1];
+            }
+            H_data[bin] += 1;
+        }
+    }
+
+    goto success;
+
+success:
+    Py_XDECREF(bins_np);
+    Py_DECREF(sample_np);
+    return Py_BuildValue("NN", H, edges);
+
+fail:
+    Py_XDECREF(bins_np);
+    Py_XDECREF(sample_np);
+    Py_XDECREF(edges);
+    Py_XDECREF(H);
     return NULL;
 }
 
 PyMethodDef methods[] = {
-    {"histogram", (PyCFunction) histogram, METH_VARARGS | METH_KEYWORDS, "Method docstring"},
+    {
+        "histogram",
+        (PyCFunction) histogram,
+        METH_VARARGS | METH_KEYWORDS,
+        "Method docstring"
+    },
+    {
+        "histogramdd",
+        (PyCFunction) histogramdd,
+        METH_VARARGS | METH_KEYWORDS,
+        "Method docstring"
+    },
     {NULL, NULL, 0, NULL} /* sentinel */
 };
 
--- tests/test_mephisto.py
+++ tests/test_mephisto.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pytest
-from hypothesis import given, strategies as st
+from hypothesis import given, settings, strategies as st
 
 import mephisto as mp
 
 
-def test_a_ndarray():
+def test_histogram_a_ndarray():
     a = np.sin(np.linspace(0, 10*np.pi, 1000))
     hist_np, bin_edges_np = np.histogram(a)
     hist_mp, bin_edges_mp = mp.histogram(a)
@@ -20,7 +20,7 @@ def test_a_ndarray():
     assert np.array_equal(hist_np,  hist_mp)
 
 
-def test_a_list():
+def test_histogram_a_list():
     a = [1, 1, 2, 2, 3]
     hist_np, bin_edges_np = np.histogram(a)
     hist_mp, bin_edges_mp = mp.histogram(a)
@@ -35,8 +35,8 @@ def test_a_list():
     assert np.array_equal(hist_np, hist_mp)
 
 
-@given(st.integers(min_value=1, max_value=2**16))
-def test_bins_scalar(bins):
+@given(st.integers(min_value=1, max_value=16))
+def test_histogram_bins_scalar(bins):
     rng = np.random.RandomState(42)
     a = rng.normal(size=1000)
     hist_np, bin_edges_np = np.histogram(a, bins)
@@ -53,18 +53,79 @@ def test_bins_scalar(bins):
 
 
 @pytest.mark.xfail
-def test_bins_array():
+def test_histogram_bins_array():
     rng = np.random.RandomState(42)
     a = rng.normal(size=1000)
     bins = np.linspace(-5, 5, 10)
-    hist_np, bin_edges_np = np.histogram(a, bins)
     hist_mp, bin_edges_mp = mp.histogram(a, bins)
-    assert bin_edges_np.dtype == bin_edges_mp.dtype
-    assert bin_edges_np.ndim == bin_edges_mp.ndim
-    assert bin_edges_np.shape == bin_edges_mp.shape
-    assert np.array_equal(bin_edges_np, bin_edges_mp)
-    assert hist_np.dtype == hist_mp.dtype
-    assert hist_np.ndim == hist_mp.ndim
-    assert hist_np.shape == hist_mp.shape
-    assert hist_np.sum() == hist_mp.sum()
-    assert np.array_equal(hist_np,  hist_mp)
+
+
+def test_histogramdd_sample_ndarray():
+    rng = np.random.RandomState(42)
+    sample = rng.randn(100, 3)
+    H_np, edges_np = np.histogramdd(sample)
+    H_mp, edges_mp = mp.histogramdd(sample)
+    assert type(edges_np) == type(edges_mp)
+    assert len(edges_np) == len(edges_mp)
+    assert all(n.dtype == m.dtype for n, m in zip(edges_np, edges_mp))
+    assert all(n.ndim == m.ndim for n, m in zip(edges_np, edges_mp))
+    assert all(n.shape == m.shape for n, m in zip(edges_np, edges_mp))
+    assert all(np.array_equal(n, m) for n, m in zip(edges_np, edges_mp))
+    assert H_np.dtype == H_mp.dtype
+    assert H_np.ndim == H_mp.ndim
+    assert H_np.shape == H_mp.shape
+    assert H_np.sum() == H_mp.sum()
+    assert np.array_equal(H_np,  H_mp)
+
+
+@pytest.mark.xfail
+def test_histogramdd_sample_list():
+    sample = [[1, 1, 2, 2, 3], [1, 2, 2, 3, 3], [2, 2, 3, 3, 4]]
+    H_mp, edges_mp = mp.histogramdd(sample)
+
+
+@given(st.integers(min_value=1, max_value=16))
+def test_histogramdd_bins_scalar(bins):
+    rng = np.random.RandomState(42)
+    sample = rng.randn(100, 3)
+    H_np, edges_np = np.histogramdd(sample, bins)
+    H_mp, edges_mp = mp.histogramdd(sample, bins)
+    assert type(edges_np) == type(edges_mp)
+    assert len(edges_np) == len(edges_mp)
+    assert all(n.dtype == m.dtype for n, m in zip(edges_np, edges_mp))
+    assert all(n.ndim == m.ndim for n, m in zip(edges_np, edges_mp))
+    assert all(n.shape == m.shape for n, m in zip(edges_np, edges_mp))
+    assert all(np.array_equal(n, m) for n, m in zip(edges_np, edges_mp))
+    assert H_np.dtype == H_mp.dtype
+    assert H_np.ndim == H_mp.ndim
+    assert H_np.shape == H_mp.shape
+    assert H_np.sum() == H_mp.sum()
+    assert np.array_equal(H_np,  H_mp)
+
+
+@settings(deadline=None)
+@given(st.lists(st.integers(min_value=1, max_value=8), min_size=2, max_size=8))
+def test_histogramdd_bins_array_of_scalars(bins):
+    rng = np.random.RandomState(42)
+    sample = rng.randn(100//len(bins), len(bins))
+    H_np, edges_np = np.histogramdd(sample, bins)
+    H_mp, edges_mp = mp.histogramdd(sample, bins)
+    assert type(edges_np) == type(edges_mp)
+    assert len(edges_np) == len(edges_mp)
+    assert all(n.dtype == m.dtype for n, m in zip(edges_np, edges_mp))
+    assert all(n.ndim == m.ndim for n, m in zip(edges_np, edges_mp))
+    assert all(n.shape == m.shape for n, m in zip(edges_np, edges_mp))
+    assert all(np.array_equal(n, m) for n, m in zip(edges_np, edges_mp))
+    assert H_np.dtype == H_mp.dtype
+    assert H_np.ndim == H_mp.ndim
+    assert H_np.shape == H_mp.shape
+    assert H_np.sum() == H_mp.sum()
+    assert np.array_equal(H_np,  H_mp)
+
+
+@pytest.mark.xfail
+def test_histogramdd_bins_narray():
+    rng = np.random.RandomState(42)
+    sample = rng.randn(100, 3)
+    bins = (np.arange(3), np.arange(5), np.arange(7))
+    H_mp, edges_mp = mp.histogramdd(sample, bins)
